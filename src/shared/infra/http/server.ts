30ba5184d534bd62f30dec4c3373507a235f19fe
César Augusto Costa <cezarcozta@gmail.com>
2020-07-26 23:29:22 -0300

    delivery static files

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 
 import express from 'express';
 import 'express-async-errors';
-
 import cors from 'cors';
+import uploadConfig from '@config/upload';
 
 import routes from './routes';
 
@@ -14,8 +14,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('Backend started on port 3333');
 });
